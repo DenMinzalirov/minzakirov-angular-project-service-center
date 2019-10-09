609fc78d8de86ab4a4d0367883aa93e1446a8044
Den Minzakirov <Den.Minzakirov@gmail.com>
2019-10-09 22:58:29 +0300

    feat: inpliment func in order-view

--- serviceCenter/src/app/order-view/order-view.component.ts
+++ serviceCenter/src/app/order-view/order-view.component.ts
@@ -7,51 +7,44 @@ import {map} from "rxjs/operators";
   templateUrl: './order-view.component.html',
   styleUrls: ['./order-view.component.css']
 })
-export class OrderViewComponent implements OnInit, DoCheck {
+export class OrderViewComponent implements OnInit{
   currentMonth = new Date().toLocaleDateString().substring(3).replace(/\./g,"-");
   listMonths;
+  numberOrder;
   allBase;
   orderInput = 1;
   listOrder = [{
     numberOrder: 1,
-    dateOrder: 1
+    dateOrder: 1,
+    modelPhone: '',
+    brandPhone: ''
   }];
 
   constructor(private firebaseService: FirebaseService) { }
 
   ngOnInit() {
-    this.firebaseService.load().subscribe(x => this.allBase = x);
-
-    // this.firebaseService.load().pipe(
-    //   map(x => Object.keys(x))
-    // ).subscribe(x => this.listMonths = x);
-
-  }
-  isObject = function(a) {
-    return a instanceof Object && a.constructor === Object;
-    // return (!!a) && (a.constructor === Object);
-  };
-
-  ngDoCheck(): void {
-    if (this.isObject(this.allBase) && this.allBase !== null){
-      this.listMonths = Object.keys(this.allBase);
-      this.listOrder = Object.values(this.allBase[this.currentMonth]);
-    };
-
-    // this.temp = Object.values(this.allBase);
+    this.firebaseService.load().subscribe(x => {
+      this.allBase = x;
+      this.listMonths = Object.keys(x);
+      this.listOrder = Object.values(x[this.currentMonth]);
+    });
   }
 
-  onRequest(orderInput) {
-    console.log(orderInput);
-    console.log(this.listOrder[orderInput - 1]);
-    console.log(this.listOrder);
+  selectCurrentMonth(event) {
+    this.currentMonth = event;
+    this.listOrder = Object.values(this.allBase[event])
+  }
+
+  selectCurrentOrder(event) {
+    this.orderInput = event;
+    console.log(this.orderInput)
   }
 
   check() {
-    // this.firebaseService.load().pipe(
-    //   map(x => Object.keys(x))
-    // ).subscribe(x => console.log(x));
-    console.log(this.allBase)
+
+    console.log(this.currentMonth);
+    console.log(this.listOrder);
+    console.log(this.listMonths)
   }
 
 }
